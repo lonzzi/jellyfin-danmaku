27cec533a883047a035cad74677480b70c2ddcc4
Xarth-Mai <72293030+Xarth-Mai@users.noreply.github.com>
2023-11-20 12:57:02 +0800

    Filtering_AutoReload
    Filtering_AutoReload

--- ede.js
+++ ede.js
@@ -120,7 +120,7 @@
         };
 
         const filterButtonOpts = {
-            title: '过滤等级(下次加载生效)',
+            title: '过滤等级',
             id: 'filteringDanmaku',
             //innerText: null,
             class: '',
@@ -130,6 +130,8 @@
                 level = ((level ? parseInt(level) : 0) + 1) % 4;
                 window.localStorage.setItem('danmakuFilterLevel', level);
                 document.querySelector('#filteringDanmaku').children[0].className = spanClass + filter_icons[level];
+                reloadDanmaku('reload');
+                
             },
         };
 
